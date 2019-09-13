9b7bff81d9a1e5fd191b8309a04c8688c0416fbb
Branislav Bily <branislav.bily@gmail.com>
2019-09-13 15:22:54 +0200

    FEAT Using MVVM

--- app/src/main/java/com/pixelart/notedock/activity/MainActivity.kt
+++ app/src/main/java/com/pixelart/notedock/activity/MainActivity.kt
@@ -1,12 +1,26 @@
-package com.pixelart.notedock
+package com.pixelart.notedock.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import com.pixelart.notedock.R
+import com.pixelart.notedock.viewModel.MainViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel = MainViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        observeLiveData()
+    }
+
+    private fun observeLiveData() {
+        mainViewModel.userNameLd.observe(this, Observer {
+            textField.text = it
+        })
     }
 }
