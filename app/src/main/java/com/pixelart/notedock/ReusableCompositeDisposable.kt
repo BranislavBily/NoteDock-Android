917a550053364161f857dec6a8ba97940b91a5dd
Branislav Bily <branislav.bily@gmail.com>
2019-11-16 12:58:27 +0100

    FEAT RXJava

---
+++ app/src/main/java/com/pixelart/notedock/ReusableCompositeDisposable.kt
@@ -0,0 +1,23 @@
+package com.pixelart.notedock
+
+import io.reactivex.disposables.CompositeDisposable
+
+class ReusableCompositeDisposable(
+    private val onObserveCallback: ((CompositeDisposable) -> Unit)? = null
+) {
+    private val lock = Unit
+    var disposeBag: CompositeDisposable? = null
+        private set
+
+    fun dispose() = synchronized(lock) {
+        disposeBag?.dispose()
+        disposeBag = null
+    }
+
+    fun observe() {
+        dispose()
+        val newBag = CompositeDisposable()
+        disposeBag = newBag
+        onObserveCallback?.invoke(newBag)
+    }
+}
