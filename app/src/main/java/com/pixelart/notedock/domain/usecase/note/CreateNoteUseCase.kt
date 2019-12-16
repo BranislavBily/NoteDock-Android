423452bdbdd14941f26974d1d4893493461fc16b
Branislav Bily <branislav.bily@gmail.com>
2019-12-16 20:01:25 +0100

    FEAT Creating Untitled note

--- app/src/main/java/com/pixelart/notedock/domain/usecase/note/CreateNoteUseCase.kt
+++ app/src/main/java/com/pixelart/notedock/domain/usecase/note/CreateNoteUseCase.kt
@@ -2,28 +2,27 @@ package com.pixelart.notedock.domain.usecase.note
 
 import com.google.firebase.firestore.FirebaseFirestore
 import com.pixelart.notedock.domain.repository.FirebaseIDSRepository
-import com.pixelart.notedock.model.NoteModel
-import com.pixelart.notedock.viewModel.SaveNoteEvent
 import io.reactivex.Single
 
-interface AddNoteUseCase {
-    fun addNote(folderUUID: String, note: NoteModel): Single<SaveNoteEvent>
+interface CreateNoteUseCase {
+    fun createNote(folderUUID: String): Single<String>
 }
 
-class AddNoteImpl(private val firebaseIDSRepository: FirebaseIDSRepository,
-                  private val firebaseInstance: FirebaseFirestore): AddNoteUseCase {
-    override fun addNote(folderUUID: String, note: NoteModel): Single<SaveNoteEvent> {
-        return Single.create<SaveNoteEvent> {emitter ->
+class CreateNoteImpl(private val firebaseIDSRepository: FirebaseIDSRepository,
+                     private val firebaseInstance: FirebaseFirestore): CreateNoteUseCase {
+    override fun createNote(folderUUID: String): Single<String> {
+        return Single.create<String> {emitter ->
             val data = hashMapOf(
-                firebaseIDSRepository.getNoteTitle() to note.noteTitle,
-                firebaseIDSRepository.getNoteDescription() to note.noteDescription
+                firebaseIDSRepository.getNoteTitle() to "Untitled"
             )
 
             firebaseInstance.collection(firebaseIDSRepository.getCollectionFolders())
                 .document(folderUUID)
                 .collection(firebaseIDSRepository.getCollectionNotes())
                 .add(data)
-                .addOnSuccessListener { emitter.onSuccess(SaveNoteEvent.Success) }
+                .addOnSuccessListener { documentReference ->
+                    emitter.onSuccess(documentReference.id)
+                 }
                 .addOnFailureListener { emitter.onError(it)}
         }
     }
