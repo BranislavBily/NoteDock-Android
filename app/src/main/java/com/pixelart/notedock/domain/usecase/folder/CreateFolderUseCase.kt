62609b5714502d4b2758c045bd38db2b8f3c859f
Branislav Bily <branislav.bily@gmail.com>
2019-12-18 16:23:58 +0100

    FEAT NotesCount in number

--- app/src/main/java/com/pixelart/notedock/domain/usecase/folder/CreateFolderUseCase.kt
+++ app/src/main/java/com/pixelart/notedock/domain/usecase/folder/CreateFolderUseCase.kt
@@ -5,18 +5,18 @@ import com.pixelart.notedock.domain.repository.FirebaseIDSRepository
 import com.pixelart.notedock.model.FolderModel
 import io.reactivex.Single
 
-interface AddFolderUseCase {
-    fun addFolder(folder: FolderModel): Single<String>
+interface CreateFolderUseCase {
+    fun createFolder(folder: FolderModel): Single<String>
 }
 
-class AddFolderImpl(private val firebaseIDSRepository: FirebaseIDSRepository,
-                    private val firebaseInstance: FirebaseFirestore): AddFolderUseCase {
+class CreateFolderImpl(private val firebaseIDSRepository: FirebaseIDSRepository,
+                       private val firebaseInstance: FirebaseFirestore): CreateFolderUseCase {
 
-    override fun addFolder(folder: FolderModel): Single<String> {
+    override fun createFolder(folder: FolderModel): Single<String> {
         return Single.create { emitter ->
             val data = hashMapOf(
                 firebaseIDSRepository.getFolderName() to folder.name,
-                firebaseIDSRepository.getFolderNotesCount() to "0"
+                firebaseIDSRepository.getFolderNotesCount() to 0
             )
 
             firebaseInstance.collection(firebaseIDSRepository.getCollectionFolders())
