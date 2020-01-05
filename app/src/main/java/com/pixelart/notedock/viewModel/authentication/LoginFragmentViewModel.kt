c7c75fa4c7bf9dc6edd1a17fb57df027af25d845
Branislav Bily <branislav.bily@gmail.com>
2020-01-05 16:08:26 +0100

    FEAT Enable disable button

--- app/src/main/java/com/pixelart/notedock/viewModel/authentication/LoginFragmentViewModel.kt
+++ app/src/main/java/com/pixelart/notedock/viewModel/authentication/LoginFragmentViewModel.kt
@@ -1,6 +1,8 @@
 package com.pixelart.notedock.viewModel.authentication
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import com.pixelart.notedock.dataBinding.SingleLiveEvent
 import com.pixelart.notedock.dataBinding.rxjava.LifecycleViewModel
 
@@ -9,9 +11,24 @@ class LoginFragmentViewModel: LifecycleViewModel() {
     private val _loginEvent = SingleLiveEvent<LoginEvent>()
     val loginEvent: LiveData<LoginEvent> = _loginEvent
 
-    fun login() {
+    val email = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
+
+    val loginEnabled: LiveData<Boolean> = MediatorLiveData<Boolean>().apply {
+        addSource(email) {
+            postValue(it.isNotEmpty() && password.value?.isNotEmpty() == true)
+        }
+        addSource(password) {
+            postValue(it.isNotEmpty() && email.value?.isNotEmpty() == true)
+        }
+    }
+
+    fun loginButtonPressed() {
         _loginEvent.postValue(LoginEvent.Success)
     }
+
+
+
 }
 
 sealed class LoginEvent {
