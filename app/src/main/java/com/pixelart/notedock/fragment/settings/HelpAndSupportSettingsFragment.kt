f375f807af9c1be901ad8d288e3a1f1a647dd6f4
Branislav Bily <branislavbily@Branislavs-iMac.local>
2020-02-06 16:09:56 +0100

    FEAT Basic layout with click listener

--- app/src/main/java/com/pixelart/notedock/fragment/settings/HelpAndSupportSettingsFragment.kt
+++ app/src/main/java/com/pixelart/notedock/fragment/settings/HelpAndSupportSettingsFragment.kt
@@ -1,20 +1,26 @@
 package com.pixelart.notedock.fragment.settings
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.pixelart.notedock.BR
 import com.pixelart.notedock.R
+import com.pixelart.notedock.adapter.SettingsAdapter
 import com.pixelart.notedock.dataBinding.setupDataBinding
 import com.pixelart.notedock.domain.livedata.observer.EventObserver
+import com.pixelart.notedock.model.SettingsModel
 import com.pixelart.notedock.viewModel.settings.HelpAndSupportViewModel
+import kotlinx.android.synthetic.main.fragment_help_and_support_settings.*
 import org.koin.android.viewmodel.ext.android.viewModel
+import java.util.*
 
-class HelpAndSupportSettingsFragment : Fragment() {
+class HelpAndSupportSettingsFragment : Fragment(), SettingsAdapter.OnSettingsClickListener {
 
     private val helpAndSupportViewModel: HelpAndSupportViewModel by viewModel()
 
@@ -34,12 +40,29 @@ class HelpAndSupportSettingsFragment : Fragment() {
     override fun onStart() {
         super.onStart()
 
+        val settingsAdapter = SettingsAdapter(createSettings(), this)
+        recycler_view_help_and_support.layoutManager = LinearLayoutManager(context)
+        recycler_view_help_and_support.adapter = settingsAdapter
         observeLiveData()
+    }
+
+    private fun createSettings(): ArrayList<SettingsModel> {
+        val settings = ArrayList<SettingsModel>()
+        settings.add(SettingsModel(R.drawable.ic_bug, getString(R.string.send_bug_report)))
+        settings.add(SettingsModel(R.drawable.ic_feedback, getString(R.string.send_feedback)))
+        return settings
     }
 
     private fun observeLiveData() {
         helpAndSupportViewModel.onBackClicked.observe(viewLifecycleOwner, EventObserver {
             findNavController().popBackStack()
         })
+    }
+
+    override fun onSettingClick(setting: SettingsModel) {
+        when(setting.title) {
+            getString(R.string.send_bug_report) -> {Log.i("Tag", "Bug report")}
+            getString(R.string.send_feedback) -> {Log.i("Tag", "Feedback report")}
+        }
     }
 }
