900e912f0009646768fe0d82c348423168a63710
Branislav Bily <branislavbily@Branislavs-iMac.local>
2020-02-06 16:32:15 +0100

    FEAT Sending email, showing whole bunch of apps

--- app/src/main/java/com/pixelart/notedock/fragment/settings/HelpAndSupportSettingsFragment.kt
+++ app/src/main/java/com/pixelart/notedock/fragment/settings/HelpAndSupportSettingsFragment.kt
@@ -1,24 +1,31 @@
 package com.pixelart.notedock.fragment.settings
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.protobuf.LazyStringArrayList
 import com.pixelart.notedock.BR
 import com.pixelart.notedock.R
 import com.pixelart.notedock.adapter.SettingsAdapter
 import com.pixelart.notedock.dataBinding.setupDataBinding
 import com.pixelart.notedock.domain.livedata.observer.EventObserver
+import com.pixelart.notedock.ext.showAsSnackBar
 import com.pixelart.notedock.model.SettingsModel
 import com.pixelart.notedock.viewModel.settings.HelpAndSupportViewModel
 import kotlinx.android.synthetic.main.fragment_help_and_support_settings.*
 import org.koin.android.viewmodel.ext.android.viewModel
+import java.lang.StringBuilder
 import java.util.*
+
 
 class HelpAndSupportSettingsFragment : Fragment(), SettingsAdapter.OnSettingsClickListener {
 
@@ -60,9 +67,22 @@ class HelpAndSupportSettingsFragment : Fragment(), SettingsAdapter.OnSettingsCli
     }
 
     override fun onSettingClick(setting: SettingsModel) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.putExtra(Intent.EXTRA_EMAIL, Array(1) { "branislav.bily@gmail.com"})
+        intent.type = "message/rfc822"
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         when(setting.title) {
-            getString(R.string.send_bug_report) -> {Log.i("Tag", "Bug report")}
-            getString(R.string.send_feedback) -> {Log.i("Tag", "Feedback report")}
+            getString(R.string.send_bug_report) -> {
+                intent.putExtra(Intent.EXTRA_SUBJECT, "NoteDock: Bug report")
+            }
+            getString(R.string.send_feedback) -> {
+                intent.putExtra(Intent.EXTRA_SUBJECT, "NoteDock: Feedback")
+            }
+        }
+        try {
+            startActivity(Intent.createChooser(intent, "Send mail..."))
+        } catch (e: ActivityNotFoundException) {
+            view?.let { R.string.no_email_client_installed.showAsSnackBar(it) }
         }
     }
 }
