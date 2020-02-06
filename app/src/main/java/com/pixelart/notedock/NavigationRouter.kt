8ce28eb77e8941a52957d6cc6132ff82c119d20f
Branislav Bily <branislavbily@Branislavs-iMac.local>
2020-02-06 13:08:33 +0100

    FEAT NavigationRouter for Folders

--- app/src/main/java/com/pixelart/notedock/NavigationRouter.kt
+++ app/src/main/java/com/pixelart/notedock/NavigationRouter.kt
@@ -4,6 +4,7 @@ import android.view.View
 import androidx.navigation.NavDirections
 import androidx.navigation.Navigation
 import com.pixelart.notedock.fragment.folder.FolderFragmentDirections
+import com.pixelart.notedock.fragment.folder.FoldersViewFragmentDirections
 import com.pixelart.notedock.fragment.settings.SettingsFragmentDirections
 
 class NavigationRouter(private val view: View?) {
@@ -38,6 +39,20 @@ class NavigationRouter(private val view: View?) {
             val navigationController = Navigation.findNavController(view)
             navigationController.navigate(action)
         }
+    }
+
+    fun foldersToSettings() {
+        view?.let { view ->
+            val action = FoldersViewFragmentDirections.actionFoldersViewFragmentToSettingsFragment()
+            val navigationRouter = NavigationRouter(view)
+            navigationRouter.openAction(action)
+        }
+    }
+
+    fun foldersToFolder(folderUUID: String, folderName: String) {
+        val action = FoldersViewFragmentDirections.actionFoldersViewFragmentToFolderFragment(folderUUID, folderName)
+        val navigationRouter = NavigationRouter(view)
+        navigationRouter.openAction(action)
     }
 
     fun openAction(action: NavDirections) {
