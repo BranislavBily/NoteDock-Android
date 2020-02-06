2c4b71aec80fda03a68bdae53d639d1e10ecacba
Branislav Bily <branislavbily@Branislavs-iMac.local>
2020-02-06 14:48:24 +0100

    FEAT Saving button enabled only when all fields are filled

--- app/src/main/java/com/pixelart/notedock/fragment/settings/ChangePasswordSettingsFragment.kt
+++ app/src/main/java/com/pixelart/notedock/fragment/settings/ChangePasswordSettingsFragment.kt
@@ -6,12 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.pixelart.notedock.BR
 import com.pixelart.notedock.R
 import com.pixelart.notedock.dataBinding.setupDataBinding
 import com.pixelart.notedock.domain.livedata.observer.EventObserver
 import com.pixelart.notedock.viewModel.settings.ChangePasswordViewModel
+import kotlinx.android.synthetic.main.fragment_change_password_settings.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class ChangePasswordSettingsFragment : Fragment() {
@@ -34,12 +36,29 @@ class ChangePasswordSettingsFragment : Fragment() {
     override fun onStart() {
         super.onStart()
 
+        setupToolbar()
         observeLiveData()
+    }
+
+    private fun setupToolbar() {
+        toolbarChangePassword?.setOnMenuItemClickListener {menuItem ->
+            when(menuItem.itemId) {
+                R.id.menu_item_save -> {
+                    changePasswordViewModel.saveNewPassword()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     private fun observeLiveData() {
         changePasswordViewModel.onBackClicked.observe(viewLifecycleOwner, EventObserver {
             findNavController().popBackStack()
+        })
+
+        changePasswordViewModel.savePasswordEnabled.observe(viewLifecycleOwner, Observer { enabled ->
+            toolbarChangePassword?.menu?.getItem(0)?.isEnabled = enabled
         })
     }
 }
--- app/src/main/java/com/pixelart/notedock/viewModel/settings/ChangePasswordViewModel.kt
+++ app/src/main/java/com/pixelart/notedock/viewModel/settings/ChangePasswordViewModel.kt
@@ -1,6 +1,8 @@
 package com.pixelart.notedock.viewModel.settings
 
+import android.util.Log
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.auth.FirebaseAuth
 import com.pixelart.notedock.dataBinding.rxjava.LifecycleViewModel
@@ -10,6 +12,30 @@ class ChangePasswordViewModel(private val auth: FirebaseAuth) : LifecycleViewMod
 
     private val _onBackClicked = MutableLiveData<ButtonPressedEvent>()
     val onBackClicked: LiveData<ButtonPressedEvent> = _onBackClicked
+
+    val currentPassword = MutableLiveData<String>()
+    val newPassword = MutableLiveData<String>()
+    val confirmNewPassword = MutableLiveData<String>()
+
+    val savePasswordEnabled: LiveData<Boolean> = MediatorLiveData<Boolean>().apply {
+        addSource(currentPassword) {
+            postValue(it.isNotEmpty() && newPassword.value?.isNotEmpty() == true && confirmNewPassword.value?.isNotEmpty() == true)
+        }
+
+        addSource(newPassword) {
+            postValue(it.isNotEmpty() && currentPassword.value?.isNotEmpty() == true && confirmNewPassword.value?.isNotEmpty() == true)
+        }
+
+        addSource(confirmNewPassword) {
+            postValue(it.isNotEmpty() && currentPassword.value?.isNotEmpty() == true && newPassword.value?.isNotEmpty() == true)
+        }
+    }
+
+    fun saveNewPassword() {
+        if(savePasswordEnabled.value == true) {
+            Log.i("Saveing", "HAhahaha")
+        }
+    }
 
     fun onBackPressed() {
         _onBackClicked.postValue(ButtonPressedEvent.Pressed())
