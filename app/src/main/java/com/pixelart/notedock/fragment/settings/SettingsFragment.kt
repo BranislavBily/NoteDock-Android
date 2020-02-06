fd6ee7778929b3f534b6b8d328a8c04c3b4d78ef
Branislav Bily <branislavbily@Branislavs-iMac.local>
2020-02-06 11:41:20 +0100

    FEAT Simple Settings with recycler view

--- app/src/main/java/com/pixelart/notedock/fragment/settings/SettingsFragment.kt
+++ app/src/main/java/com/pixelart/notedock/fragment/settings/SettingsFragment.kt
@@ -3,22 +3,27 @@ package com.pixelart.notedock.fragment.settings
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.ViewDataBinding
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.pixelart.notedock.BR
 
 import com.pixelart.notedock.R
 import com.pixelart.notedock.activity.LoginActivity
+import com.pixelart.notedock.adapter.SettingsAdapter
 import com.pixelart.notedock.dataBinding.setupDataBinding
 import com.pixelart.notedock.domain.livedata.observer.EventObserver
+import com.pixelart.notedock.model.SettingsModel
 import com.pixelart.notedock.viewModel.settings.SettingsFragmentViewModel
+import kotlinx.android.synthetic.main.fragment_settings.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : Fragment(), SettingsAdapter.OnSettingsClickListener {
 
     private val settingFragmentViewModel: SettingsFragmentViewModel by viewModel()
 
@@ -39,6 +44,9 @@ class SettingsFragment : Fragment() {
         super.onStart()
 
         observeLiveData()
+        val settingsAdapter = SettingsAdapter( createSettings(), this)
+        recyclerViewSettings.layoutManager = LinearLayoutManager(context)
+        recyclerViewSettings.adapter = settingsAdapter
     }
 
     private fun observeLiveData() {
@@ -55,5 +63,15 @@ class SettingsFragment : Fragment() {
                 activity?.overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
             }
         })
+    }
+
+    override fun onSettingClick(setting: SettingsModel) {
+        Log.i("Settings", "Clickclickclick")
+    }
+
+    private fun createSettings(): ArrayList<SettingsModel> {
+        val settings = ArrayList<SettingsModel>()
+        settings.add(SettingsModel(R.drawable.ic_logout, "Log out"))
+        return settings
     }
 }
---
+++ app/src/main/java/com/pixelart/notedock/model/SettingsModel.kt
@@ -0,0 +1,3 @@
+package com.pixelart.notedock.model
+
+data class SettingsModel(val drawable: Int,val title: String)
