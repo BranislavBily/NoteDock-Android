544c98ee124e6e22cc9e3753241b436897063d94
Branislav Bily <branislavbily@Branislavs-iMac.local>
2020-02-07 17:16:01 +0100

    FEAT Basic layout

--- app/src/main/java/com/pixelart/notedock/viewModel/settings/DeleteAccountViewModel.kt
+++ app/src/main/java/com/pixelart/notedock/viewModel/settings/DeleteAccountViewModel.kt
@@ -1,6 +1,8 @@
 package com.pixelart.notedock.viewModel.settings
 
+import android.util.Log
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.auth.FirebaseAuth
 import com.pixelart.notedock.dataBinding.rxjava.LifecycleViewModel
@@ -11,7 +13,22 @@ class DeleteAccountViewModel(private val auth: FirebaseAuth) : LifecycleViewMode
     private val _onBackClicked = MutableLiveData<ButtonPressedEvent>()
     val onBackClicked: LiveData<ButtonPressedEvent> = _onBackClicked
 
+    private val _loading = MutableLiveData<Boolean>()
+    val loading: LiveData<Boolean> = _loading
+
+    val password = MutableLiveData<String>()
+
+    val deleteAccountEnabled: LiveData<Boolean> = MediatorLiveData<Boolean>().apply {
+        addSource(password) {
+            postValue(it.isNotEmpty())
+        }
+    }
+
     fun onBackPressed() {
         _onBackClicked.postValue(ButtonPressedEvent.Pressed())
+    }
+
+    fun deleteAccount() {
+        Log.i("Delete", password.value)
     }
 }
