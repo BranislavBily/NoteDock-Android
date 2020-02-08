90b08db13678d8de59967ca7914a1e43df7b7525
Branislav Bily <branislavbily@Branislavs-iMac.local>
2020-02-08 12:11:20 +0100

    FEAT ChangeEmail basic layout

--- app/src/main/java/com/pixelart/notedock/viewModel/settings/ChangeEmailViewModel.kt
+++ app/src/main/java/com/pixelart/notedock/viewModel/settings/ChangeEmailViewModel.kt
@@ -1,6 +1,8 @@
 package com.pixelart.notedock.viewModel.settings
 
+import android.util.Log
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.auth.FirebaseAuth
 import com.pixelart.notedock.dataBinding.rxjava.LifecycleViewModel
@@ -11,7 +13,23 @@ class ChangeEmailViewModel(private val auth: FirebaseAuth) : LifecycleViewModel(
     private val _onBackClicked = MutableLiveData<ButtonPressedEvent>()
     val onBackClicked: LiveData<ButtonPressedEvent> = _onBackClicked
 
+    var email = MutableLiveData<String>()
+    var password = MutableLiveData<String>()
+
+    val changeEmailEnabled: LiveData<Boolean> = MediatorLiveData<Boolean>().apply {
+        addSource(email) {
+            postValue(it.isNotEmpty() &&  password.value?.isNotEmpty() == true)
+        }
+        addSource(password) {
+            postValue(it.isNotEmpty() &&  email.value?.isNotEmpty() == true)
+        }
+    }
+
     fun onBackPressed() {
         _onBackClicked.postValue(ButtonPressedEvent.Pressed())
+    }
+
+    fun changeEmailClick() {
+        Log.i("Change email", "Clicked")
     }
 }
