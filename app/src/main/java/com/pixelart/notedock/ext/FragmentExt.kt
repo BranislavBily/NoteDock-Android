21b0d25ed04b862dfb244fdbfcc5470f6026d841
Branislav Bily <branislavbily@Branislavs-iMac.local>
2020-02-08 14:09:19 +0100

    FEAT Extension to gotoLogin

--- app/src/main/java/com/pixelart/notedock/ext/FragmentExt.kt
+++ app/src/main/java/com/pixelart/notedock/ext/FragmentExt.kt
@@ -2,9 +2,12 @@ package com.pixelart.notedock.ext
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
+import com.pixelart.notedock.R
+import com.pixelart.notedock.activity.LoginActivity
 
 fun Fragment.hideSoftKeyboard(context: Context, view: View) {
     val imm: InputMethodManager = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -16,4 +19,11 @@ fun Fragment.openSoftKeyBoard(activity: Activity) {
     val methodManager = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     assert(view != null)
     methodManager.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+}
+
+fun Fragment.openLoginActivity() {
+    val intent = Intent(context, LoginActivity::class.java)
+    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+    startActivity(intent)
+    activity?.overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
 }
