11ba4939fe031d9bc5592466db8cf2dd64fc427f
Branislav Bily <branislavbily@Branislavs-iMac.local>
2020-02-11 16:26:38 +0100

    FEAT GenericCRUDEvent

--- app/src/main/java/com/pixelart/notedock/viewModel/folder/FolderFragmentViewModel.kt
+++ app/src/main/java/com/pixelart/notedock/viewModel/folder/FolderFragmentViewModel.kt
@@ -13,6 +13,7 @@ import com.pixelart.notedock.domain.usecase.note.DeleteNoteUseCase
 import com.pixelart.notedock.domain.usecase.note.MarkNoteUseCase
 import com.pixelart.notedock.model.NoteModel
 import com.pixelart.notedock.viewModel.authentication.ButtonPressedEvent
+import com.pixelart.notedock.viewModel.note.GenericCRUDEvent
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.schedulers.Schedulers
@@ -34,8 +35,8 @@ class FolderFragmentViewModel(
     private val _onBackClicked = MutableLiveData<ButtonPressedEvent>()
     val onBackClicked: LiveData<ButtonPressedEvent> = _onBackClicked
 
-    private val _deleteNote = MutableLiveData<DeleteNoteEvent>()
-    val deleteNote: LiveData<DeleteNoteEvent> = _deleteNote
+    private val _deleteNote = MutableLiveData<GenericCRUDEvent>()
+    val deleteNote: LiveData<GenericCRUDEvent> = _deleteNote
 
     private val _fabClicked = MutableLiveData<ButtonPressedEvent>()
     val fabClicked: LiveData<ButtonPressedEvent> = _fabClicked
@@ -43,8 +44,8 @@ class FolderFragmentViewModel(
     private val _noteCreated = MutableLiveData<CreateNoteEvent>()
     val noteCreated: LiveData<CreateNoteEvent> = _noteCreated
 
-    private val _markNote = MutableLiveData<MarkNoteEvent>()
-    val markNote: LiveData<MarkNoteEvent> = _markNote
+    private val _markNote = MutableLiveData<GenericCRUDEvent>()
+    val markNote: LiveData<GenericCRUDEvent> = _markNote
 
     override fun onStartStopObserve(disposeBag: CompositeDisposable) {
         super.onStartStopObserve(disposeBag)
@@ -86,10 +87,10 @@ class FolderFragmentViewModel(
                     .subscribeOn(Schedulers.io())
                     .observeOn(Schedulers.io())
                     .subscribe(
-                        { _markNote.postValue(MarkNoteEvent.Success()) },
+                        { _markNote.postValue(GenericCRUDEvent.Success()) },
                         { error ->
                             Crashlytics.logException(error)
-                            _markNote.postValue(MarkNoteEvent.Error())
+                            _markNote.postValue(GenericCRUDEvent.Error())
                         }
                     )
                     .addTo(bag)
@@ -104,16 +105,16 @@ class FolderFragmentViewModel(
                     .subscribeOn(Schedulers.io())
                     .observeOn(Schedulers.io())
                     .subscribe(
-                        { _deleteNote.postValue(DeleteNoteEvent.Success()) },
+                        { _deleteNote.postValue(GenericCRUDEvent.Success()) },
                         { error ->
                             Crashlytics.logException(error)
-                            _deleteNote.postValue(DeleteNoteEvent.Error())
+                            _deleteNote.postValue(GenericCRUDEvent.Error())
                         }
                     )
                     .addTo(bag)
             }
         } ?: run {
-            _deleteNote.postValue(DeleteNoteEvent.NoUserFoundError())
+            _deleteNote.postValue(GenericCRUDEvent.NoUserFound())
         }
     }
 
@@ -157,16 +158,4 @@ sealed class LoadNotesEvent : Event() {
         LoadNotesEvent()
     class Error : LoadNotesEvent()
     class NoUserFoundError : LoadNotesEvent()
-}
-
-sealed class DeleteNoteEvent: Event() {
-    class Success: DeleteNoteEvent()
-    class Error: DeleteNoteEvent()
-    class NoUserFoundError: DeleteNoteEvent()
-}
-
-sealed class MarkNoteEvent: Event() {
-    class Success: MarkNoteEvent()
-    class Error: MarkNoteEvent()
-    class NoUserFoundError: MarkNoteEvent()
 }
